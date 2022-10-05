8d853200ec16fbdc2c8fe9397803b87a52afd5dd
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-05 17:03:28 -0500

    Post route complete

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,8 +1,10 @@
 import React from 'react';
+import ShelfPageForm from './ShelfPageForm';
 
 function ShelfPage() {
   return (
     <div className="container">
+      <ShelfPageForm />
       <h2>Shelf</h2>
       <p>All of the available items can be seen here.</p>
     </div>
