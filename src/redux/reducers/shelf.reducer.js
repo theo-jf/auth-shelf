d5e5b2b311a322164dc11ca6feea528aafeac6ad
Theo Janke-Furman <tjankefurman@gmail.com>
2022-10-05 16:27:33 -0500

    Created shelf reducer

---
+++ src/redux/reducers/shelf.reducer.js
@@ -0,0 +1,17 @@
+// SHELF REDUCER IN HERE
+import { combineReducers } from 'redux';
+
+const shelf = (state = [], action) => {
+    switch (action.type) {
+        case 'SET_SHELF':
+            return action.payload
+    }
+    return state
+}
+
+
+
+
+export default combineReducers({
+    shelf
+  });
