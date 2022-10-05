ab40fa3c85baa37c469f560ac761a5e52ed71d44
Sam Freeman <samcfreeman888@gmail.com>
2022-10-05 17:01:57 -0500

    get route works.  Haven't rendered it yet

--- src/components/ShelfPage/ShelfPage.js
+++ src/components/ShelfPage/ShelfPage.js
@@ -1,6 +1,15 @@
-import React from 'react';
+
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
 
 function ShelfPage() {
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch({ type: 'SAGA_GET_SHELF' });
+}, []);
+
+  
   return (
     <div className="container">
       <h2>Shelf</h2>
